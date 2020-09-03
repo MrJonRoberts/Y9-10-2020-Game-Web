05d69f64a603a00f3f95e0dc11065c0fa9ad2b4d
Jon Roberts <jroberts@tas.qld.edu.au>
2020-09-03 14:27:51 +1000

    calc

--- scripts/main.js
+++ scripts/main.js
@@ -1,58 +1,54 @@
-// button demo
+// calc button demo
+
+var display;
+var fns;
+var actions;
+var nums;
 
 
-
-// 1.  something to do when we click a button
-// 2.  add an event handler to the button click.
-
-var clickCount = 0;
-var health = 100;
-
-//1 
 function consoleLogger(msg){
 
     document.getElementById("messages").innerHTML = msg;
 }
 
-function backstab(){
-
-    if (health > 0){
-        damage = Math.floor(Math.random() * 10 +1)
-        health = health - damage;
-        consoleLogger("YOU DID "+ damage + " damage ("+ health + ")");
-
-        document.getElementById('hp').value = health;
-
+function start(){
+    // set up the page.
+    var nums = document.getElementsByClassName("num");
+    for (i=0; i< nums.length; i ++){
+         nums[i].addEventListener('click', getNum);
     }
-    if (health <= 0){
-        
-        document.getElementById('restart').disabled = false;
-        document.getElementById('name').disabled = false;
-        consoleLogger("YOU ARE DEAD");
+    
+    var actions = document.getElementsByClassName("action");
+    for (i=0; i< actions.length; i++){
+         actions[i].addEventListener('click', getFn);
     }
+    var fns = document.getElementsByClassName("fn");
+     for (i=0; i< fns.length; i++){
+         fns[i].addEventListener('click', getActions);
+    }
+    display = document.getElementById('display');
+    
 }
 
-function changeB3(){
-    clickCount = clickCount + 1;
-    document.getElementById("b3").innerHTML = "B3 Clicked " + clickCount +" times";
+function getNum(){
+    consoleLogger(this.innerHTML); 
+    theNum = this.innerHTML;
+    display.append(theNum + "\n")
 }
 
-function restart(){
-    health = 100;
-    name = document.getElementById('name').value;
-    alert("ready: " + name);
-    document.getElementById('hp').value = health;
-    document.getElementById("name").disabled = true;
-    document.getElementById("restart").disabled = true;
-    consoleLogger("RESTARTING");
+function getFn(){
+    var theFn = this.InnerHtml;
+    if (theFn == "C"){
+        clearDisplay();
+    }
+    consoleLogger(this.innerHTML) 
 }
-//2 
+function getactions(){
+    
+    consoleLogger(this.innerHTML) 
+}
+function clearDisplay(){
+    display.innerHTML = "";
+}
 
-document.getElementById("b1").addEventListener('click', function(){ consoleLogger("B1 Clicked")});
-document.getElementById("b2").addEventListener('click', function(){ alert("B2 Clicked")});
-document.getElementById("b3").addEventListener('click', changeB3);
-document.getElementById('bstab').addEventListener('click', backstab);
-document.getElementById('restart').addEventListener('click', restart);
-document.getElementById('hp').value = health;
-document.getElementById('restart').disabled = true;
-restart();
+start();
